1895f594f2c9fd1fa1c827db76d7c2abd25e5980
Soohyun Ahn <soahn@c8r8s10.42seoul.kr>
2021-06-25 15:23:08 +0900

    1st

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soahn <soahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 12:41:53 by soahn             #+#    #+#             */
+/*   Updated: 2021/06/25 13:52:17 by soahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+int		get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *src);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soahn <soahn@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 12:41:49 by soahn             #+#    #+#             */
+/*   Updated: 2021/06/25 13:34:47 by soahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	i = 0;
+	while (src[i] && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dest[i] = '\0';
+	return (src_len);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dest_size;
+
+	if (size == 0)
+		return (ft_strlen(src));
+	dest_size = ft_strlen(dest);
+	if (size < dest_size)
+		return (ft_strlen(src) + size);
+	i = dest_size;
+	j = 0;
+	while (src[j] && i < (size - 1))
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (ft_strlen(src) + dest_size);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*res;
+
+	if (!(s1) || !(s2))
+	{
+		if (!(s1))
+			return (ft_strdup(s2));
+		return (ft_strdup(s1));
+	}
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (NULL);
+	ft_strlcpy(res, s1, s1_len + 1);
+	free(s1);
+	ft_strlcpy(res + s1_len, s2, s2_len + 1);
+	return (res);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	size_t	i;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (!(dest = (char *)malloc(sizeof(char) * src_len + 1)))
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
